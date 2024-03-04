8fbd10d414ee85bf43105887b55df47768da51ed
asligg <162028201+asligg@users.noreply.github.com>
2024-03-04 18:40:55 +0300

    Update SivasKosu.java

--- SivasKosu.java
+++ SivasKosu.java
@@ -1,14 +1,19 @@
 public class SivasKosu {
+    public SivasKosu() {
+    }
+
     public static void main(String[] args) {
 
-        String[] ogrenciler = {"Kadir", "Gökhan", "Hakan", "Suzan", "Pınar", "Mehmet", "Ali", "Emel",
-                                "Fırat", "James", "Jale", "Ersin", "Deniz", "Gözde", "Anıl", "Burak"};
-        int[] dakika = {341, 273, 278, 329, 445, 402, 388, 270, 243, 334, 412, 393, 299, 343, 317, 265};
+        String[] isimler = {"Kadir", "Gökhan", "Hakan", "Suzan", "Pınar", "Mehmet", "Ali", "Emel"
+                , "Fırat", "James", "Jale", "Ersin", "Deniz", "Gözde", "Anıl", "Burak"};
 
-        KosuYarismasi.ilkUcOgrenciyiBul(ogrenciler, dakika);
-        KosuYarismasi.ogrenciGruplandirma(dakika);
+        int[] sure = {341, 273, 278, 329, 445, 402, 388, 270, 243, 334, 412, 393, 299, 343, 317, 265};
+
+        KosuYarismasi kosuyarismasi = new KosuYarismasi(sure,isimler);
+
+        kosuyarismasi.getUcuncu();
+
+        kosuyarismasi.Siniflandir();
 
     }
 }
-
-
